8742560bbb63f86a3852948c2f55984e41c95130
dym061 <dominik.vonfrancois@hotmail.com>
2023-03-02 00:02:24 +0200

    Replacing | with . in Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const year = today.getFullYear();
 
 const Footer = () => (
   <footer className='footer txtfont2 txtsize3'>
-	brewed in <span className='text--strong'> Next.js </span> | <span className='text--strong'> React </span> | <span> {year} </span>
+	brewed in <span className='text--strong'> Next.js </span> . <span className='text--strong'> React </span> | <span> {year} </span>
   </footer>
 )
 
